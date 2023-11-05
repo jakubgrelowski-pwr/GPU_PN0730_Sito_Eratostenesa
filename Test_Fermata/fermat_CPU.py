c2ba3473e610e03dffef7e0347849f3d5573ed09
Bartosz Kloc <atlf75800@gmail.com>
2023-11-05 18:01:42 +0100

    (Fermat CPU): first attempt to multithreading (errors with 0)

--- Test_Fermata/fermat_CPU.py
+++ Test_Fermata/fermat_CPU.py
@@ -1,34 +1,53 @@
+import threading
 import random
 
-# wykonanie obliczenia:
+# Losowa liczba i wykonanie obliczenia:
 def power_mod(base, exponent, mod):
     result = 1
+
     while exponent > 0:
         if exponent % 2 == 1:
             result = (result * base) % mod
         base = (base * base) % mod
         exponent //= 2
-
-    print("wybrano a=", base, ", wynik: ", result)
+    
     return result
 
-# Glowna funkcja generujaca losowe liczby 'a' i powtarzajaca obliczenia:
-def is_prime(p, repetiton):
-    if p <= 1:
-        return False
-
-    for _ in range(repetition):
-        a = random.randint(2, 1000)
-        if power_mod(a, p - 1, p) != 1:
-            return False
-
-    return True
-
 # 561 - liczba Carmichaela dla ktorej wychodzi roznie
-p = 561
-repetition = 4
+p = 3
 
-if is_prime(p, repetition):
+# Liczba powtórzeń:
+num_threads = 4
+
+# Lista na wyniki
+results = []
+base_a = []
+
+for i in range(num_threads):
+    base_a.append(random.randint(2, 1000))
+
+print(base_a)
+
+# Tworzenie wątków
+threads = []
+for i in range(num_threads):
+    thread = threading.Thread(target=lambda i=i: results.append(power_mod(base_a[i], p-1,p)))
+    threads.append(thread)
+    thread.start()
+
+# Oczekiwanie na zakończenie wątków
+for thread in threads:
+    thread.join()
+
+# Sprawdzenie wynikow ze wszyskich powtórzeń:
+print("Wyniki z wszystkich wątków:", results)
+is_prime = True
+
+for i in results:
+    if i!=1:
+        is_prime = False
+
+if is_prime:
     print(p, " jest prawdopodobnie l. pierwsza")
 else:
     print(p, " jest prawdopodobnie l. zlozona")
