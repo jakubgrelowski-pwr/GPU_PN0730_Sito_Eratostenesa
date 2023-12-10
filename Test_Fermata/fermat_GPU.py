a66a128ff1e70ca3531406cc36561d74e7b3873d
MTorski <148274006+MTorski@users.noreply.github.com>
2023-12-10 18:37:23 +0100

    Fermat GPU tests update

--- Test_Fermata/fermat_GPU.py
+++ Test_Fermata/fermat_GPU.py
@@ -1,61 +1,75 @@
 import pyopencl as cl
 import numpy as np
-import cProfile
+import random
+import time
 
-def fermat_test(n, a_values, ctx, queue):
-    kernel_code = """
-    __kernel void fermat_test(__global ulong* results, ulong n, __global ulong* a_values, int num_tests) {
-        int gid = get_global_id(0);
-        ulong x = 1;
-        for (ulong i = 0; i < n - 1; i++) {
-            x = (x * a_values[gid]) % n;
-        }
-        results[gid] = (x == 1) ? 1 : 0;
+platform = cl.get_platforms()[0]
+device = platform.get_devices()[0]
+context = cl.Context([device])
+queue = cl.CommandQueue(context, properties=cl.command_queue_properties.PROFILING_ENABLE)
+
+kernel_code = """
+__kernel void fermat_test(__global ulong* results, ulong n, __global ulong* a_values, int num_tests) {
+    int gid = get_global_id(0);
+    ulong x = 1;
+    for (ulong i = 0; i < n - 1; i++) {
+        x = (x * a_values[gid]) % n;
     }
-    """
+    results[gid] = (x == 1) ? 1 : 0;
+}
+"""
 
-    prg = cl.Program(ctx, kernel_code).build()
+prg = cl.Program(context, kernel_code).build()
 
-    results = np.zeros(len(a_values), dtype=np.uint64)
+
+
+def fermat_test_gpu(n, k, ctx, queue):
+
+    results = np.zeros(k, dtype=np.uint64)
+    a_values = np.random.randint(2, n, size=k, dtype=np.uint64)
+
     a_values_buffer = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=a_values)
     results_buffer = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY, results.nbytes)
 
-    prg.fermat_test(queue, (len(a_values),), None, results_buffer, np.uint64(n), a_values_buffer, np.uint32(len(a_values)))
+    prg.fermat_test(queue, (k,), None, results_buffer, np.uint64(n), a_values_buffer, np.uint32(k))
 
-    # Czytanie wynikow z bufora
+    # Read the results from the buffer
     cl.enqueue_copy(queue, results, results_buffer).wait()
 
-    return results
+    return all(result == 1 for result in results)
 
 def main():
-    # Liczba do testowania (n)
-    n = 1000037
 
-    # Liczba testow i generowanie losowych wartosci
-    num_tests = 100000
-    a_values = np.random.randint(2, n, size=num_tests, dtype=np.uint64)
+    carmichael_numbers = [633267, 414154, 622188, 269938, 788018, 676447, 858479, 201674, 996040, 1000037]
 
-    # Start pomiaru:
-    profiler = cProfile.Profile()
-    profiler.enable()
+    number_ranges = {
+        "1001-9999": (1001, 9999),
+        "10001-99999": (10001, 99999),
+        "100001-999999": (100001, 999999),
+        "Carmichael": carmichael_numbers
+    }
 
-    platform = cl.get_platforms()[0]
-    device = platform.get_devices()[0]
-    ctx = cl.Context([device])
-    queue = cl.CommandQueue(ctx)
+    k = 10000
+    num_repeats = 100
 
-    results = fermat_test(n, a_values, ctx, queue)
+    for range_name, number_range in number_ranges.items():
+        total_execution_time = 0.0
 
-    # Pomiar
-    profiler.disable()
-    profiler.print_stats()
+        for _ in range(num_repeats):
+            n = random.randint(*number_range) if range_name != "Carmichael" else random.choice(carmichael_numbers)
 
-    # Wyniki
-    # for i, result in enumerate(results):
-    #     if result == 1:
-    #         print(f"Dla a = {a_values[i]}, {n} jest liczba prawdopodobnie pierwsza.")
-    #     else:
-    #         print(f"For a = {a_values[i]}, {n} jest liczba zlozona.")
+            start_time = time.time()
+            is_prime = fermat_test_gpu(n, k, context, queue)
+            end_time = time.time()
+
+            execution_time = end_time - start_time
+            total_execution_time += execution_time
+
+            print(f"Test fermata gpu, test {_ + 1}/{num_repeats}, czas: {execution_time:.4f}s, rezultat: {n} is prime: {is_prime}")
+
+        average_execution_time = total_execution_time / num_repeats
+
+        print(f"Range: {range_name}, Number of Repeats: {num_repeats}, Average Execution Time: {average_execution_time:.10f} seconds\n")
 
 if __name__ == "__main__":
     main()
